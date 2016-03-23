0b803899eb58486f44396dba2d2bdc7f5ea393c8
Hristo Deshev <hristo@deshev.com>
2016-03-23 11:25:39 +0200

    Add a Map<K, V> type definition.

--- d.ts/references.d.ts
+++ d.ts/references.d.ts
@@ -1,2 +1,23 @@
 /// <reference path="android17.d.ts" />
 /// <reference path="ios.d.ts" />
+
+interface Map<K, V> {
+    clear(): void;
+    delete(key: K): boolean;
+    forEach(callbackfn: (value: V, index: K, map: Map<K, V>) => void, thisArg?: any): void;
+    get(key: K): V;
+    has(key: K): boolean;
+    set(key: K, value: V): Map<K, V>;
+    size: number;
+    keys(): Array<K>;
+    values(): Array<V>;
+}
+declare var Map: {
+    new <K, V>(): Map<K, V>;
+
+    // needed by Angular
+    // alexeagle: PATCHED
+    new<K, V>(m: Map<K, V>): Map<K, V>;
+    new<K, V>(l: Array<any>): Map<K, V>;
+    prototype: Map<any, any>;
+}
